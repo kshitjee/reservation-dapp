a3fb11f9c5520ff4348d418562aff33e0cd77d5c
Soham Jog <jogsoham2003@gmail.com>
2023-02-10 15:43:49 -0500

    Imported open zeppelin

--- contracts/Auction_2.sol
+++ contracts/Auction_2.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Auction {
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract Auction_2 is ERC1155 {
     // The address that owns the contract
     address owner;
 
-    // The NFT that is up for auction
-    uint256 tokenId;
+    // The ERC-1155 NFTs that are up for auction
+    uint256[] nftIds;
 
     // The current highest bid
     uint256 highestBid;
@@ -22,13 +24,13 @@ contract Auction {
     event AuctionEnd(address winner, uint256 winningBid);
 
     // Constructor function to initialize the auction
-    constructor(uint256 _tokenId, uint256 _auctionEndTime) public {
+    constructor(uint256[] memory _nftIds, uint256 _auctionEndTime) {
         owner = msg.sender;
-        tokenId = _tokenId;
+        nftIds = _nftIds;
         auctionEndTime = _auctionEndTime;
     }
 
-    // Bid on the NFT
+    // Bid on the ERC-1155 NFT
     function bid(uint256 _bid) public payable {
         require(msg.value > highestBid, "Bid must be higher than the current highest bid");
         require(block.timestamp < auctionEndTime, "Auction has already ended");
@@ -39,12 +41,16 @@ contract Auction {
         emit NewBid(highestBid, highestBidder);
     }
 
-    // End the auction and award the NFT to the highest bidder
-    function endAuction() public payable {
+    // End the auction and award the ERC-1155 NFT to the highest bidder
+    function endAuction() public {
         require(block.timestamp >= auctionEndTime, "Auction has not ended yet");
         require(msg.sender == owner, "Only the owner can end the auction");
 
-        highestBidder.transfer(msg.value);
+        for (uint256 i = 0; i < nftIds.length; i++) {
+            _safeTransferFrom(owner, highestBidder, nftIds[i], 0, "");
+        }
+
+        highestBidder.transfer(highestBid);
 
         emit AuctionEnd(highestBidder, highestBid);
     }
