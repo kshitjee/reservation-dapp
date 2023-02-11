848cb29780c53bc7bc13df0b81211f61ed881875
DeveshRamiya <68691515+DeveshRamiya@users.noreply.github.com>
2023-02-11 00:57:58 -0500

    ngh

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import Header from '../components/Header.js'
 
 export default function Home() {
-
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Header
+      heading="Login to your account"
+      paragraph="Don't have an account yet? "
+      linkName="Signup"
+      linkUrl="/signup"
+    />
   )
-
 }
