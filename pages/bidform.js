d8c16e0c66c184f8bbb6dceb01131eb8aa906865
Soham Jog <jogsoham2003@gmail.com>
2023-02-11 22:34:36 -0500

    useeffect

--- pages/bidform.js
+++ pages/bidform.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import BidForm from '../components/Makebid';
 
+//get all the data from the auction and set a const to it
+
+
+
+
 function BidPage() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch(`http://localhost:5001/api/auctions/`, {
+                method: "Get",
+                mode: 'cors',
+                headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+                contentType: "application/json"
+            });
+            const data = await response.json();
+            setData(data);
+        }
+        fetchData();
+    }, []);
+
+
+    console.log(data)
     const sampleData = {
         eventDescription: 'Auction for a rare painting',
         location: 'New York',
